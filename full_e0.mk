f22e6bd9ff3872741e0561f42d7a81f9b7664e7c
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2024-10-06 15:03:58 +0200

    Update full_e0.mk

--- full_e0.mk
+++ full_e0.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/e0/device.mk)
+$(call inherit-product, device/lge/e400/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := full_e0
