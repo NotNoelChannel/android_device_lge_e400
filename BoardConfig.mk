b26f0f3b36708ea3015f3f145dc50bda5a43fb36
NotNoelChannel <notnoel12345678@gmail.com>
2024-10-07 16:43:21 +0200

    refactor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,17 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Define device specific variables to msm7x27a-common
-LGE_MSM7X27A_DEVICE := v1
-LGE_MSM7X27A_GENERATION := 2
-BOARD_VENDOR_PLATFORM := v1
-
 # inherit from the proprietary things
-include device/lge/msm7x27a-common/BoardConfigCommon.mk
+include device/lge/e0-common/BoardConfigCommon.mk
 include vendor/lge/v1/BoardConfigVendor.mk
 
 # Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_e0_defconfig
+TARGET_KERNEL_CONFIG := e0_android_defconfig
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
@@ -36,6 +31,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 164626432
 DEVICE_RESOLUTION := 320x240
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 RECOVERY_FSTAB_VERSION := 2
+TARGET_RECOVERY_FSTAB := device/lge/e0/rootdir/root/fstab.e0
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 BOARD_USES_MMCUTILS := true
@@ -44,7 +40,7 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 
 # TWRP
 #RECOVERY_VARIANT := omni
-TW_EXTERNAL_STORAGE_PATH := "/external sd"
+TW_EXTERNAL_STORAGE_PATH := "/sdcard"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_FLASH_FROM_STORAGE := true
